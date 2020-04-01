ed24d6def81658db59ea40abcd6a27557b5f8dca
nsk917 <kengo.n917@gmail.com>
2020-04-01 20:15:06 +0900

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -6,7 +6,10 @@ git_source(:github) do |repo_name|
 end
 
 gem 'rails',        '5.1.7'
-gem 'bcrypt',         '3.1.12'
+gem 'bcrypt',       '3.1.12'
+gem 'faker',        '1.7.3'
+gem 'will_paginate', '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
 gem 'bootstrap-sass', '3.3.7'
 gem 'puma',         '3.12.4'
 gem 'sass-rails',   '5.0.6'
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -15,5 +19,21 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+
+  test "layout links for logged in user" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user.id)
+    assert_select "a[href=?]", edit_user_path(@user.id)
+    assert_select "a[href=?]", logout_path
+
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", "http://news.railstutorial.org/"
   end
 end
